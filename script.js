a0b157a4700bc43ecd293eee2264c1fcef9c54ec
EluciOwl <github.yen839@passmail.net>
2026-06-16 20:35:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const thoughtButton =
+  document.querySelector("#thought-button")
+
+const analyzeButton =
+  document.querySelector("#analyze-button")
+
+
+analyzeButton.addEventListener("click", () => {
+  analyzeButton.textContent = "Grr"
+});
+
+thoughtButton.addEventListener("click", () => {
+  thoughtButton.classList.toggle("active")
+  if (thoughtButton.classList.contains("active")) {
+    thoughtButton.textContent = "Gedanke";
+  } else {
+    thoughtButton.textContent = "Miau";
+    alert("Grrrr");
+  }
+});
+
