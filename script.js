3ff397ac8c7b5b1205fe599dab440394658b91c7
EluciOwl <Celvius@protonmail.com>
2026-06-27 22:34:38 +0200

    Store and recreate thoughts on emotions.html
    Refactor scripts.js into functions
    - Organize style.css

--- script.js
+++ script.js
@@ -1,135 +1,71 @@
-// I get it! -> when I'm on thoughts.html and klick on "home-thougts-button" he'll find it GREAT!
-// BUT!!! -> at the same time on thoughts.html he CAN'T find "home-button" and "thought-button" because it's on "index.html"
-// SOOO... -> the first line of code doesen't work when I'm on "thoughts.html" -> returns null -> CRASH -> code below will never happen
-// consequently: click on home-thoughts-button nothing will happen!!!
-// solution: I have to check first -> is it there? (YES)-> then do... / (NO)-> skip that part move to next code below 
-
-// index.html
-const homeButton = document.getElementById("home-button");
-if (homeButton) {
-  // function() {...} (shorter way)-> () => {...}
-  homeButton.addEventListener("click", () => {
-    // klicked -> go to index.html
-    window.location.href = "index.html";
-  });
-}
-// doesen't exist? -> just skip 
-
-const thoughtsButton = document.getElementById("thoughts-button");
-if (thoughtsButton) {
-  thoughtsButton.addEventListener("click", () => {
-    window.location.href = "thoughts.html";
-  });
-}
-
-// thoughts.html buttons
-const homeThoughtButton = document.querySelector(".home-purple-button");
-if (homeThoughtButton) {
-  homeThoughtButton.addEventListener("click", () => {
-    window.location.href = "index.html";
-  });
-}
-
-// cointainer to collect all thought-inputs
-const thoughtsContainer = document.getElementById("thoughts-container");
-// Thoughts from input
-const thoughtInput = document.querySelector("#thought-input-box");
-const thoughtCounter = document.getElementById("thought-counter");
+// Thoughts from input field
+const thoughtInput = document.getElementById("thought-input-box");
+// readyButton that appears after 4 entered thoughts
 const readyButton = document.getElementById("ready-button");
+// collecting thoughts via input
+let thoughtsArray = [];
 
-// when thoughtInput -> this code can run, not? -> skip
-if (thoughtInput) {
+const MAX_THOUGHTS = 4;
 
-  inputCounter = 0;
-  
-  // "=" -> sets value | "===" is this equal to...?"
-  // listen for "keydown"
-  // I GET IT ---> "hey thoughtInput, whenever a key goes down, give me info about that event, and if the key was Enter → run this code"
-
-  // set color of thoughtCounter -> WHY DOES IT ONYL WORK AT THAT PLAY WITH COLOR AT BEGINNING???
-  thoughtCounter.style.color = "rgba(204, 73, 255, 0.5)";
-  thoughtCounter.style.filter = "saturate(1)";
-  thoughtInput.addEventListener("keydown", (event) => {
-    
-  // when browser gives me Enter -> div, img, span
-  if (event.key === "Enter" && inputCounter < 4) {
-    inputCounter++;
-
-    // thoughtCounter raises every cycle
-    thoughtCounter.textContent = inputCounter.toString() + "/4";
-    
-    // thoughtCounter will wobble and goes green when 4/4
-    if (inputCounter > 3) {
-     thoughtCounter.style.color = "rgba(204, 73, 255, 0.5)";
-     thoughtCounter.style.filter = "brightness(1.3)";
-     thoughtCounter.classList.add("wobble");
-    }
-    else if (inputCounter > 2){
-     thoughtCounter.style.color = "rgba(204, 73, 255, 0.5)";
-     thoughtCounter.style.filter = "brightness(1.2)";
-    }
-    else if (inputCounter > 1) {
-     thoughtCounter.style.color = "rgba(204, 73, 255, 0.5)";
-     thoughtCounter.style.filter = "brightness(1.1)";
-    }
-    else {
-     thoughtCounter.style.color = "rgba(204, 73, 255, 0.5)";
-     thoughtCounter.style.filter = "brightness(1)";
-    }
-
-    // Container for all Clouds
-    const divClouds = document.createElement("div");
-    divClouds.classList.add("thought-cloud");
-
-    // Cloud image create
-    const imgCloud = document.createElement("img");
-    imgCloud.src = "assets/images/ai-generated/thought.png";
-
-    // Text create
-    const thoughtTextCloud = document.createElement("span");
-    thoughtTextCloud.classList.add("thought-text-cloud");
-    thoughtTextCloud.textContent = thoughtInput.value;
-    // clearing the text-field
-    thoughtInput.value = "";
-
-    // put created img, text into Container
-    divClouds.appendChild(imgCloud);
-    divClouds.appendChild(thoughtTextCloud);
-    thoughtsContainer.appendChild(divClouds); 
+// Naviagtions
+function IndexNavigation() {
+  const homeButton = document.getElementById("home-button");
+  if (homeButton) {
+    homeButton.addEventListener("click", () => {
+      window.location.href = "index.html";
+    });
   }
-  if (inputCounter === 4) {
-      readyButton.style.visibility="visible";
+
+  const thoughtsButton = document.getElementById("thoughts-button");
+  if (thoughtsButton) {
+    thoughtsButton.addEventListener("click", () => {
+      window.location.href = "thoughts.html";
+    });
   }
-  
-});
+
+  // thoughts.html buttons
+  const homeThoughtButton = document.querySelector(".home-colored-button");
+  if (homeThoughtButton) {
+    homeThoughtButton.addEventListener("click", () => {
+      window.location.href = "index.html";
+    });
+  }
+}
+function readyToEmotionsNavigation() {
+  // move from readyButton -> emotions.html
+  if (readyButton) {
+    readyButton.addEventListener("click", () => {
+      window.location.href = "emotions.html";
+    })
+  }
 }
 
-if (thoughtInput) {
-// creating the Text inside the textbox letter by letter
-const fullText = "What's on your mind?";
-let letters = 0;
+// Visual effects
+function appearingInputText() {
+  if (thoughtInput) {
+    // creating the Text inside the textbox letter by letter
+    const fullText = "What's on your mind?";
+    let letters = 0;
 
-// runs every 120ms
-const typing = setInterval(() => {
-  // from position 0, cut letters -> (0,3) = "Wha"
-  thoughtInput.placeholder = fullText.slice(0, letters);
-  letters++
+    // runs every 120ms
+    const typing = setInterval(() => {
+      // from position 0, cut letters -> (0,3) = "Wha"
+      thoughtInput.placeholder = fullText.slice(0, letters);
+      letters++
 
-  // cut letters more than the whole Text?
-  if (letters > fullText.length) {
-    clearInterval(typing);
+      // cut letters more than the whole Text?
+      if (letters > fullText.length) {
+        clearInterval(typing);
+      }
+    }, 120);
   }
-}, 120);
 }
+function sparkleEffect(sparkleing) {
 
-// creats sparcle effect for readyButton
-const sparcleEffect = document.getElementById("sparcle-effect");
+  let sparkleInterval = null;
 
-if(sparcleEffect) {
-  let sparcleIntervall = null;
-
-  sparcleEffect.addEventListener("mouseenter", () => {
-    sparcleIntervall = setInterval(() => {
+  sparkleing.addEventListener("mouseenter", () => {
+    sparkleInterval = setInterval(() => {
       const particleDot = document.createElement("div");
       // give particleDot the class: "particle" used in style.css
       particleDot.classList.add("particle")
@@ -139,20 +75,125 @@ if(sparcleEffect) {
       particleDot.style.bottom = "0px";
       particleDot.style.background = "rgb(204, 73, 255)";
       // ad it to stage
-      sparcleEffect.appendChild(particleDot);
+      sparkleing.appendChild(particleDot);
       setTimeout(() => particleDot.remove(), 1000);
     }, 50);
   });
 
-  sparcleEffect.addEventListener("mouseleave", () => {
-    clearInterval(sparcleIntervall);
+  sparkleing.addEventListener("mouseleave", () => {
+    clearInterval(sparkleInterval);
   })
+}
+function counterAppearing(thoughtCounter, inputCounter) {
+  // thoughtCounter raises every cycle
+  thoughtCounter.textContent = inputCounter.toString() + "/" + MAX_THOUGHTS.toString();
+
+  // thoughtCounter will wobble and goes green when MAX_THOUGHTS
+  if (inputCounter > 3) {
+    thoughtCounter.style.filter = "brightness(1.3)";
+    thoughtCounter.classList.add("wobble");
+  }
+  else if (inputCounter > 2) {
+    thoughtCounter.style.filter = "brightness(1.2)";
+  }
+  else if (inputCounter > 1) {
+    thoughtCounter.style.filter = "brightness(1.1)";
+  }
+  else {
+    thoughtCounter.style.filter = "brightness(1)";
+  }
 }
 
-// move from readyButton -> emotions.html
-if (readyButton) {
-  readyButton.addEventListener("click", () => {
-  window.location.href = "emotions.html";
-  })
+// Page feature
+function featureThoughts() {
+  // cointainer to collect all thought-inputs
+  const thoughtsContainer = document.getElementById("thoughts-container");
+  // display counter for entered thoughts
+  const thoughtCounter = document.getElementById("thought-counter");
+
+  // when thoughtInput -> this code can run, not? -> skip
+  if (thoughtInput) {
+    let inputCounter = 0;
+
+    thoughtCounter.textContent = inputCounter.toString() + "/" + MAX_THOUGHTS.toString();
+
+    // listen for "keydown"
+    thoughtInput.addEventListener("keydown", (event) => {
+
+      // when browser gives me Enter -> div, img, span
+      if (event.key === "Enter" && inputCounter < MAX_THOUGHTS) {
+        inputCounter++;
+        counterAppearing(thoughtCounter, inputCounter);
+        createFloatingClouds(thoughtInput.value, thoughtsContainer);
+        thoughtsArray.push(thoughtInput.value);
+        // my array  ->  JSON.stringify  ->  a JSON string  ->  localStorage holds it
+        localStorage.setItem("thoughts", JSON.stringify(thoughtsArray));
+        // clearing the text-field
+        thoughtInput.value = "";
+      }
+      if (inputCounter === MAX_THOUGHTS) {
+        // creats sparkle effect for readyButton
+        const sparkleVisual = document.getElementById("sparkle-effect");
+        readyButton.style.visibility = "visible";
+        sparkleEffect(sparkleVisual);
+      }
+    });
+  }
 }
+function thoughtsRecreateOnDocEmotions() {
+  const thoughtsCollectedContainer = document.getElementById("thoughts-collected-container");
+
+  if (thoughtsCollectedContainer) {
+    let thoughtsJson = localStorage.getItem("thoughts");
+    // || [] protects .length from crashing if parse gave nothing!
+    thoughtsArray = JSON.parse(thoughtsJson) || [];
+
+    for (let thoughtsNumber = 0; thoughtsNumber < thoughtsArray.length; thoughtsNumber++) {
+      createFloatingClouds(thoughtsArray[thoughtsNumber], thoughtsCollectedContainer);
+    }
+  }
+}
+
+// Creation
+function createFloatingClouds(input, container) {
+  // Container for all Clouds
+  const divClouds = document.createElement("div");
+  divClouds.classList.add("thought-cloud");
+
+  // Cloud image create
+  const imgCloud = document.createElement("img");
+  imgCloud.src = "assets/images/ai-generated/thought.png";
+
+  // Text create
+  const thoughtTextCloud = document.createElement("span");
+  thoughtTextCloud.classList.add("thought-text-cloud");
+  // .value is the String I gave -> .textContent is showing the text
+  thoughtTextCloud.textContent = input;
+
+  // put created img, text into Container
+  divClouds.appendChild(imgCloud);
+  divClouds.appendChild(thoughtTextCloud);
+  container.appendChild(divClouds);
+}
+
+//order matters inside a function, not between functions.
+function init() {
+  // Navigation
+  IndexNavigation();
+  readyToEmotionsNavigation();
+
+  // Page feature
+  featureThoughts();
+  thoughtsRecreateOnDocEmotions();
+
+  // Visual effects
+  appearingInputText();
+}
+
+// only run init when HTML is fullry parsed!  //defer -> also wait's until HTML is parsed -> covers globals  //without defer, globals -> null
+// defer makes the whole script wait for DOM! -> dealy -> saves globals form grabbing -> null
+document.addEventListener("DOMContentLoaded", init);
+
+
+
 
